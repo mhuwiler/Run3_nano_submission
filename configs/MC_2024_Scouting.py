c7eeeeba8e6ce58a1c3e53e7de30afde27c04e18
Marc Huwiler <marc.huwilercern.ch>
2025-08-21 12:37:09 +0200

    Fixing filenames in config file generation

--- configs/MC_2024_Scouting.py
+++ configs/MC_2024_Scouting.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: --python_file MC_2024_Scouting.py -s NANO:@GENFromMini+@Scout --process NANO -n 10 --nThreads 4 --era Run3_2024 --customise_commands="process.NANOAODSIMoutput.outputCommands.append()" --no_exec --eventcontent NANOAODSIM --datatier NANOAODSIM --mc --fileout file:MC_2024_Scouting.py.root --conditions auto:phase1_2024_realistic --filein /store/mc/Run3Winter24MiniAOD/GluGlutoHHto2B2Tau_kl-0p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/MINIAODSIM/133X_mcRun3_2024_realistic_v9-v3/2820000/6fde14c0-c8c4-4425-b57c-647f62654d98.root
+# with command line options: --python_file MC_2024_Scouting.py -s NANO:@GENFromMini+@Scout --process NANO -n 10 --nThreads 4 --era Run3_2024 --customise_commands="process.NANOAODSIMoutput.outputCommands.append()" --no_exec --eventcontent NANOAODSIM --datatier NANOAODSIM --mc --fileout file:MC_2024_Scouting.root --conditions auto:phase1_2024_realistic --filein /store/mc/Run3Winter24MiniAOD/GluGlutoHHto2B2Tau_kl-0p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/MINIAODSIM/133X_mcRun3_2024_realistic_v9-v3/2820000/6fde14c0-c8c4-4425-b57c-647f62654d98.root
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.Eras.Era_Run3_2024_cff import Run3_2024
@@ -81,7 +81,7 @@ process.NANOAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
         dataTier = cms.untracked.string('NANOAODSIM'),
         filterName = cms.untracked.string('')
     ),
-    fileName = cms.untracked.string('file:MC_2024_Scouting.py.root'),
+    fileName = cms.untracked.string('file:MC_2024_Scouting.root'),
     outputCommands = process.NANOAODSIMEventContent.outputCommands
 )
 
--- configs/make_configs.sh
+++ configs/make_configs.sh
@@ -58,10 +58,10 @@ cmsDriver.py $name --fileout file:$name.root --conditions $gt --filein $filein $
 
 ############# Scouting MC #############
 
-name=MC_2024_Scouting.py
+name=MC_2024_Scouting
 gt=auto:phase1_2024_realistic
 filein=/store/mc/Run3Winter24MiniAOD/GluGlutoHHto2B2Tau_kl-0p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/MINIAODSIM/133X_mcRun3_2024_realistic_v9-v3/2820000/6fde14c0-c8c4-4425-b57c-647f62654d98.root #/store/mc/Run3Winter24MiniAOD/GluGlutoHHto2B2Tau_kl-0p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/MINIAODSIM/133X_mcRun3_2024_realistic_v9-v3/2820000/a55161f6-8a99-47ed-a158-063e3726e97c.root
-cmsDriver.py --python_file $name $scouting_args_mc --fileout file:$name.root --conditions $gt --filein $filein   
+cmsDriver.py --python_file $name.py $scouting_args_mc --fileout file:$name.root --conditions $gt --filein $filein   
 
 ############# DATA #############
 
